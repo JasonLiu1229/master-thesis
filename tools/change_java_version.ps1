b5488ba9592874f54737a07ffd1e8b5ecea48090
Jason Liu <Liujason2003@gmail.com>
2025-07-27 12:22:58 +0200

    update java version script

--- tools/change_java_version.ps1
+++ tools/change_java_version.ps1
@@ -2,3 +2,6 @@ $env:JAVA_HOME = "C:\Program Files\Java\jdk-12"
 
 $env:PATH = "$env:JAVA_HOME\bin;" + ($env:PATH -replace "[^;]*\\Java\\[^;]*\\bin;?", "")
 
+java --version
+
+echo "Changed Java version to 12"
