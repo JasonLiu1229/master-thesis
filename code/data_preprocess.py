9b56d60ae0acd57111aa4f28c6a160c9a7b12bcb
Jason Liu <Liujason2003@gmail.com>
2025-08-21 15:39:48 +0200

    Add: filled in data preprocess code

--- code/data_preprocess.py
+++ code/data_preprocess.py
@@ -1,9 +1,53 @@
+from model import tokenizer
+
 
 def format(data: dict) -> str:
-    pass
+    """
+    Format the input data into a string for the model.
+    Args:
+        data (dict): Dictionary containing 'prompt' and 'response' keys.
+    Returns:
+        str: Formatted string for the model.
+    """
+    assert (
+        "prompt" in data and "response" in data
+    ), "Data must contain prompt and response keys."
 
-def tokenize(data:str):
-    pass
+    return f"You are a java identifier renamer. \n \
+        ### Obfuscated Java code: {data['prompt']} \n \
+        ### Output Java code: {data['response']}"
+
+
+def tokenize(data: dict):
+    """
+    Tokenize the input data for the model.
+    Args:
+        data (dict): Dictionary containing 'prompt' and 'response' keys.
+    Returns:
+        dict: Tokenized input and labels for the model.
+    """
+
+    assert (
+        "prompt" in data and "response" in data
+    ), "Data must contain 'prompt' and 'response' keys."
+
+    text = format(data)
+    tokens = tokenizer(
+        text,
+        truncation=True,
+        padding="max_length",
+        max_length=1024,
+        return_tensors="pt",
+    )
+    tokens["labels"] = tokens["input_ids"].copy()
+    return tokens
+
 
 def preprocess(data: list):
-    pass
+    """
+    Preprocess the data for training.
+
+    Args:
+        data (list): List of dictionaries containing the data to preprocess. So each dictionary should have 'prompt' and 'response' keys.
+    """
+    return [tokenize(item) for item in data]
