367dd5cc72167ce81335583e8af1803b22927da5
Jason Liu <Liujason2003@gmail.com>
2025-08-21 15:30:26 +0200

    add: base code and start of most the trainer

---
+++ code/data_preprocess.py
@@ -0,0 +1,9 @@
+
+def format(data: dict) -> str:
+    pass
+
+def tokenize(data:str):
+    pass
+
+def preprocess(data: list):
+    pass
---
+++ code/model.py
@@ -0,0 +1,9 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_id = "codellama/CodeLlama-7b-Instruct-hf"  # model ID for CodeLlama-7b-Instruct
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    load_in_8bit=True,  
+    device_map="auto"
+)
