69e588d143de0bdabcf4b9f073f64bb6531419dd
Jason Liu <Liujason2003@gmail.com>
2025-11-06 14:24:02 +0100

    add lora base model configuration function

--- code/tuner/tuner.py
+++ code/tuner/tuner.py
@@ -4,7 +4,6 @@ import torch
 import yaml
 from datasets import DatasetDict
 
-from model import get_model, LLM_Model
 from peft import get_peft_model, LoraConfig
 from transformers import (
     AutoModelForCausalLM,
@@ -28,7 +27,46 @@ def define_base():
         - Model
         - AutoTokenizer
     """
-    pass
+    tokenizer = AutoTokenizer.from_pretrained(config["MODEL_NAME"], use_fast=True)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        
+    if config["USE_QLORA"]:
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_type=torch.float16,
+        )
+        
+        base_model = AutoModelForCausalLM.from_pretrained(
+            config["MODEL_NAME"],
+            quantization_config=bnb_config,
+            devide_map='auto',
+            torch_dtype=torch.bfloat16 if device == "cuda" else torch.bfloat32,
+        ).to(device)
+    else:
+        base_model = AutoModelForCausalLM.from_pretrained(
+            config["MODEL_NAME"],
+            device_map='auto',
+            torch_dtype=torch.bfloat16 if device == "cuda" else torch.bfloat32,
+        ).to(device)
+    
+    lora_config = LoraConfig(
+        r=config["RANK"],
+        lora_alpha=config["LORA_ALPHA"],
+        lora_dropout=config["LORA_DROPOUT"],
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=[
+            "q_proj", "k_proj", "v_proj", "o_proj",
+            "gate_proj", "up_proj", "down_proj"
+        ],
+    )
+    
+    model = get_peft_model(base_model, lora_config)
+    return model, tokenizer  
 
 
 def tune():
