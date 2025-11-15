60ce2aeffcdbfff5c2e5691b513a0fd06eda913b
Jason Liu <Liujason2003@gmail.com>
2025-11-15 17:14:10 +0100

    start on helper class

---
+++ code/rename_pipeline/pipeline/helper.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+
+def _wrap_test_case(test_case: str) -> str:
+    """
+    Wrap the test case so it matches as similar as the original training data
+    
+    public class TestClassX {
+        @Test
+        public void func_1() { ... }
+    }
+    """
+    pass
+
+def _extract_tests_from_source(source_code: str) -> List[str]:
+    pass
+
+def extract_tests_from_file(file_path: Path) -> List[str]:
+    pass
+
+def combine_test_cases(test_cases: List[str]):
+    """
+    After the post processing of the test cases they need to be combined again to a single file
+    """
+    pass
