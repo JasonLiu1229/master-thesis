f2c4d899d8e873cc166d73edc52fe7d5c3da0e70
Jason Liu <liujason2003@gmail.com>
2025-11-19 18:26:43 +0100

    Start on text exractror

--- code/rename_pipeline/pipeline/helper.py
+++ code/rename_pipeline/pipeline/helper.py
@@ -3,7 +3,15 @@ from pathlib import Path
 from typing import List
 import json
 
-def _wrap_test_case(test_case: str) -> str:
+class JavaTestSpan: # A smaller form to save the test_cases
+    name: str
+    annotation_line: int 
+    method_line: int
+    start_line: int
+    end_line: int
+
+
+def wrap_test_case(test_case: str) -> str:
     """
     Wrap the test case so it matches as similar as the original training data
     
@@ -13,14 +21,14 @@ def _wrap_test_case(test_case: str) -> str:
     }
     """
     pass
-
-def _extract_tests_from_source(source_code: str) -> List[str]:
+    
+def _extract_tests_from_source(source_code: str) -> List[JavaTestSpan]:
     pass
 
-def extract_tests_from_file(file_path: Path) -> List[str]:
+def extract_tests_from_file(file_path: Path) -> List[JavaTestSpan]:
     assert file_path.exists(), f"File {file_path} does not exist"
     
-    test_cases = list()
+    test_cases: List[JavaTestSpan] = list()
 
     with open(file_path, 'r') as file:
         test_cases = _extract_tests_from_source(file.read())
@@ -31,4 +39,7 @@ def combine_test_cases(test_cases: List[str]):
     """
     After the post processing of the test cases they need to be combined again to a single file
     """
+    pass
+
+if __name__ == '__main__':
     pass
