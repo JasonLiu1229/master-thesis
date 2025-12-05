1fcb88fefbf21fbdcf91aa004ea028e5f2977941
Jason Liu <Liujason2003@gmail.com>
2025-12-05 14:49:49 +0100

    version pipeline drawing added

---
+++ code/utils/utils.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# File sizes in mb -> medium is in between small and big
+BIG = 200
+SMALL = 10
+
+def check_size(file: Path):
+    pass
+
+def sort_files(folder: Path):
+    pass
